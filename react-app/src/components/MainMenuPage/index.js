3fa1e0f2d947c402917642da996afe0e0e3d5dbd
sterling_hebert <sterling_hebert@outlook.com>
2023-07-13 19:54:09 -0500

    new landing and main-menu

--- react-app/src/components/MainMenuPage/index.js
+++ react-app/src/components/MainMenuPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
 import './MainMenuPage.css';
@@ -7,40 +7,61 @@ import logo from './locationIcon.png';
 
 function MainMenuPage() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleLogout = () => {
     dispatch(logout());
-  };
+    history.push('/');  };
 
   return (
     <div className="MMP-div">
-      {/* <video class="video-background" autoplay loop muted>
-    <source src="https://cdn.pixabay.com/vimeo/533374337/waterfall-69786.mp4?width=1280&hash=e7268647d9d17c0c5d5c701a8e467e956a1a5dc3" type="video/mp4"/>
-    Your browser does not support the video tag.
-  </video> */}
       <div className="MMP-logo">
         <img src={logo} alt="Location Icon" />
       </div>
       <h1 className="MMP-h1">Location Guesser</h1>
-      <div className="MMP-button-container">
-        <button className="MMP-button-play">
-          <Link to="/play/world">Play World Game</Link>
-        </button>
-        <button className="MMP-button-play">
-          <Link to="/play/famous-places">Play Famous Places</Link>
-        </button>
-        <button className="MMP-button-play">
-          <Link to="/play/united-states">Play United States</Link>
-        </button>
-        <button className="MMP-button">
-          <Link to="/social">Social</Link>
-        </button>
-        <button className="MMP-button">
-          <Link to="/my-stats">Stats</Link>
-        </button>
-        <button className="MMP-button">
-          <Link to="/leader-boards">Leaderboards</Link>
-        </button>
+      <div className="MMP-card-container">
+        <div className="MMP-card MMP-card-play">
+          <Link to="/play/world" className="MMP-card-link">
+            <div className="MMP-card-bg MMP-bg1"></div>
+            <span>Play World Game</span>
+          </Link>
+        </div>
+        <div className="MMP-card MMP-card-play">
+          <Link to="/play/famous-places" className="MMP-card-link">
+            <div className="MMP-card-bg MMP-bg2"></div>
+            <span>Play Famous Places</span>
+          </Link>
+        </div>
+        <div className="MMP-card MMP-card-play">
+          <Link to="/play/united-states" className="MMP-card-link">
+            <div className="MMP-card-bg MMP-bg3"></div>
+            <span>Play United States</span>
+          </Link>
+        </div>
+        <div className="MMP-card MMP-card-social">
+          <Link to="/social" className="MMP-card-link">
+            <div className="MMP-card-bg MMP-bg4"></div>
+            <span>Social</span>
+          </Link>
+        </div>
+        <div className="MMP-card MMP-card-stats">
+          <Link to="/my-stats" className="MMP-card-link">
+            <div className="MMP-card-bg MMP-bg5"></div>
+            <span>Stats</span>
+          </Link>
+        </div>
+        <div className="MMP-card MMP-card-leaderboards">
+          <Link to="/leader-boards" className="MMP-card-link">
+            <div className="MMP-card-bg MMP-bg6"></div>
+            <span>Leaderboards</span>
+          </Link>
+        </div>
+        <div className="MMP-card MMP-card-logout" onClick={handleLogout}>
+          <div className="MMP-card-link">
+            <div className="MMP-card-bg MMP-bg7"></div>
+            <span>Logout</span>
+          </div>
+        </div>
       </div>
     </div>
   );
